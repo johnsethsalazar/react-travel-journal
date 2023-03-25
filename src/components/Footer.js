2910b5e21530971db32910fdb57f4ba2bd1a9dfe
John Seth Salazar <salazarjohnseth@gmail.com>
2023-03-25 15:39:07 +0800

    added target blank for the footer href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer(){
     return(
         <div id="footer">
-            <small>Made by <a href="https://johnsethsalazar.netlify.app/">John Seth Salazar</a></small>
+            <small>Made by <a href="https://johnsethsalazar.netlify.app/" target="_blank" rel="noopener noreferrer">John Seth Salazar</a></small>
         </div>
     )
 }
