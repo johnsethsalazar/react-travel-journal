09ed6bfdd33b48441fc67841b8ed4dbe67fa58dd
John Seth Salazar <salazarjohnseth@gmail.com>
2023-03-25 15:37:28 +0800

    Added portfolio website as href in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer(){
     return(
         <div id="footer">
-            <small>Made by John Seth Salazar</small>
+            <small>Made by <a href="https://johnsethsalazar.netlify.app/">John Seth Salazar</a></small>
         </div>
     )
 }
